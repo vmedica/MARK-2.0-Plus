9160889d59393e706d571ea425d7af75ad38379d
VINCENZO MEDICA <v.medica@studenti.unisa.it>
2026-01-07 16:36:18 +0100

    Revisione generale ed correzioni

--- modules/analyzer/metrics_analyzer.py
+++ modules/analyzer/metrics_analyzer.py
@@ -4,7 +4,7 @@ from modules.analyzer.ml_analyzer import MLAnalyzer
 from modules.analyzer.ml_roles import AnalyzerRole
 
 class MLMetricsAnalyzer(MLAnalyzer):
-    """Analyzer dedicato al calcolo delle metriche codice."""
+    """Analyzer dedicated to calculating code metrics."""
 
     def __init__(
         self,
@@ -13,7 +13,7 @@ class MLMetricsAnalyzer(MLAnalyzer):
         filters=None,
         keyword_strategy=None
     ):
-        # chiama il costruttore base
+        # calls the base constructor
         super().__init__(
             role=role,
             library_dicts=library_dicts,
@@ -22,5 +22,5 @@ class MLMetricsAnalyzer(MLAnalyzer):
         )
 
     def check_library(self, file, **kwargs):
-        # per METRICS non serve controllare librerie ML
+        # METRICS does not need to check ML libraries
         return [], [], []
